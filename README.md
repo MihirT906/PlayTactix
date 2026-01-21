464562195032c3e4f0d14997f0b5c1f238379ee7
Mihir Thalanki <mihir.thalanki@gmail.com>
2026-01-21 09:00:30 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PlayTactix
 This app was developed as part of my submission for the SkillCorner × PySport Hackathon. It allows users to clip sports matches, annotate key moments, and generate visualizations and insights from gameplay.
+
+### Requirements
+1. NodeJS v24.13.0
+2. npm 11.6.2
---
+++ backend/main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def hello_world():
+    return {"message": "hello world"}
+
+@app.get("/hello")
+async def hello():
+    return {"message": "hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

25db0c911988a8e13e5d6cded8a5ad7fa8e9ef30
Mihir Thalanki <mihir.thalanki@gmail.com>
2026-01-21 09:08:35 -0500

    Add CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def hello_world():
     return {"message": "hello world"}
